91dc262f298397c3bf9b0fae0c4c32acfcaf125b
Vinayak Tantia <tantia.vinayak1@gmail.com>
2016-09-24 22:17:59 +0530

    Add all files

---
+++ nachos/code/test/vectorsum.c
@@ -0,0 +1,22 @@
+#include "syscall.h"
+#define SIZE 100
+int
+main()
+{
+    system_call_PrintString("Total sum: ");
+    int array[SIZE], i, sum=0;
+
+    /*system_call_PrintString("print  ");*/
+    system_call_PrintString("Total sum: ");
+    for (i=0; i<SIZE; i++) array[i] = i;
+    for (i=0; i<SIZE; i++) sum += array[i];
+    system_call_PrintString("Total sum: ");
+    system_call_PrintInt(sum);
+    system_call_PrintChar('\n');
+    system_call_PrintString("Executed instruction count: ");
+    system_call_PrintInt(system_call_GetNumInstr());
+    system_call_PrintChar('\n');
+    system_call_Exit(0);
+    return 0;
+}
+
